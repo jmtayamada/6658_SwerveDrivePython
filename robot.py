549034eccecbdae2dc9087f7f0d2fa91f11de6f2
jmtayamada <96408404+jmtayamada@users.noreply.github.com>
2024-01-30 20:01:35 -0800

    changes

--- robot.py
+++ robot.py
@@ -3,6 +3,17 @@ import wpilib as wp
 from constants import *
 from SwerveDrive import SwerveDrive
 
+from photonlibpy.photonCamera import *
+from photonlibpy.multiTargetPNPResult import *
+from photonlibpy.photonPipelineResult import *
+from photonlibpy.photonTrackedTarget import *
+
+from SwerveModule import SwerveModule
+from wpimath.kinematics import SwerveModuleState
+from wpimath.geometry import Rotation2d
+from phoenix5.sensors import CANCoder
+from rev import CANSparkMax
+from wpilib import SmartDashboard
 
 seconds = 0
 
@@ -10,18 +21,32 @@ class Robot(wp.TimedRobot):
 
     def __init__(self, period: seconds = 0.02):
         super().__init__(period)
-        # create a swerveDrive by passing in SwerveModules, and keeping swervemodules in default positions
         self.SwerveDrive = SwerveDrive()
         # create joystick
-        self.joystick = wp.Joystick(ControllerConstants.kDrivingJoystickPort)
+        self.driverStick = wp.Joystick(ControllerConstants.kDrivingJoystickPort)
+        self.helperStick = wp.Joystick(ControllerConstants.kHelperJoystickPort)
+
+        self.testSwerveModule = SwerveModule(0, 1, 1)
 
     def teleopPeriodic(self):
-        # run swerveDrive.drive function by passing in x axis, y axis, z axis form joystick, and the current angle
+        # run swerveDrive.drive function by passing in x axis, y axis, z axis form joystick
         self.SwerveDrive.drive(
-            self.joystick.getRawAxis(0), 
-            self.joystick.getRawAxis(1), 
-            self.joystick.getRawAxis(2), 
+            self.driverStick.getRawAxis(0), 
+            self.driverStick.getRawAxis(1), 
+            self.driverStick.getRawAxis(2), 
         )
+
+    def testInit(self) -> None:
+        return super().testInit()
+    
+    def testPeriodic(self) -> None:
+        x = self.driverStick.getRawAxis(0)
+        y = self.driverStick.getRawAxis(1)
+        speed = math.sqrt(x**2+y**2)
+        angle = math.atan2(y, x)
+        SmartDashboard.putNumber("Current Angle", self.testSwerveModule.getState().angle)
+        SmartDashboard.putNumber("Current Position", self.testSwerveModule.getState().speed)
+        self.testSwerveModule.testMotors(speed, angle)
 
 
 if __name__ == "__main__":
